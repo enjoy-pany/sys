0a64b98e58e2595f3d63ce8ba1fafc9e082a09dd
pany <panyang4@kingsoft.com>
2018-08-27 19:03:33 +0800

    add proxy and pbl

---
+++ src/page/otherCenter/imageList.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <vue-waterfall-easy :imgsArr="imgsArr" @scrollReachBottom="getData"></vue-waterfall-easy>
+</div>
+</template>
+<script>
+import vueWaterfallEasy from 'vue-waterfall-easy'
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      imgsArr: [],
+      group: 0
+
+    }
+  },
+  components: {
+    vueWaterfallEasy
+  },
+  methods: {
+    getData() {
+      axios.get('api/picture/findgrid?offset=0&limit=70&tag=')
+        .then(res => {
+          this.imgsArr = this.imgsArr.concat(res.data)
+          this.group++
+        })
+    },
+  },
+  created() {
+    this.getData()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import viewWrap from '@/page/main'
 import orderList from '@/page/orderCenter/orderList'
+import imgList from '@/page/otherCenter/imageList'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
           path: 'orderlist',
           name: "orderList",
           component: orderList
+        },
+        {
+          path: 'imagelist',
+          name: 'imageList',
+          component: imgList,
         }
       ]
     }
